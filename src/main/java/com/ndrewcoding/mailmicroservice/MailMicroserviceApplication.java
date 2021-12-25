09bcb8a6196a7b56ae2cdb6c9c5f35217f342d23
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-25 16:17:36 -0300

    Feat: setup the project

---
+++ src/main/java/com/ndrewcoding/mailmicroservice/MailMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ndrewcoding.mailmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ndrewcoding/mailmicroservice/MailMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndrewcoding.mailmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
