b24d6dda5cd008b8df2505866d845ccc09bc8bcf
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-25 16:36:44 -0300

    Chore: annotate EmailRepository with @Repository

--- src/main/java/com/ndrewcoding/mailmicroservice/repositories/EmailRepository.java
+++ src/main/java/com/ndrewcoding/mailmicroservice/repositories/EmailRepository.java
@@ -2,6 +2,8 @@ package com.ndrewcoding.mailmicroservice.repositories;
 
 import com.ndrewcoding.mailmicroservice.models.EmailModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmailRepository extends JpaRepository<EmailModel, Long> {
 }
