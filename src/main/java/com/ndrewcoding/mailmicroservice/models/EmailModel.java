880df520556edaacf8b71dec4b87588da820c11d
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-25 16:30:43 -0300

    Feat: add base implementation of EmailModel

--- src/main/java/com/ndrewcoding/mailmicroservice/models/EmailModel.java
+++ src/main/java/com/ndrewcoding/mailmicroservice/models/EmailModel.java
@@ -1,2 +1,31 @@
-package com.ndrewcoding.mailmicroservice.models;public class EmailModel {
+package com.ndrewcoding.mailmicroservice.models;
+
+import com.ndrewcoding.mailmicroservice.enums.EmailStatus;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "TB_EMAIL")
+public class EmailModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long emailId;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String text;
+
+    private LocalDateTime sendDateEmail;
+    private EmailStatus emailStatus;
+
 }
