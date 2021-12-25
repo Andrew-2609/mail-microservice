baecc580979e578c9db0e0ec74f4cd1506765708
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-25 18:41:29 -0300

    Feat: add JavaMailSender to actually send the emails

--- src/main/java/com/ndrewcoding/mailmicroservice/services/EmailService.java
+++ src/main/java/com/ndrewcoding/mailmicroservice/services/EmailService.java
@@ -5,6 +5,7 @@ import com.ndrewcoding.mailmicroservice.models.EmailModel;
 import com.ndrewcoding.mailmicroservice.repositories.EmailRepository;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -13,9 +14,11 @@ import java.time.LocalDateTime;
 public class EmailService {
 
     private final EmailRepository emailRepository;
+    private final JavaMailSender emailSender;
 
-    public EmailService(EmailRepository emailRepository) {
+    public EmailService(EmailRepository emailRepository, JavaMailSender emailSender) {
         this.emailRepository = emailRepository;
+        this.emailSender = emailSender;
     }
 
     public EmailModel sendEmail(EmailModel emailModel) {
@@ -26,6 +29,8 @@ public class EmailService {
             message.setTo(emailModel.getEmailTo());
             message.setSubject(emailModel.getSubject());
             message.setText(emailModel.getText());
+
+            emailSender.send(message);
 
             emailModel.setEmailStatus(EmailStatus.SENT);
         } catch (MailException me) {
