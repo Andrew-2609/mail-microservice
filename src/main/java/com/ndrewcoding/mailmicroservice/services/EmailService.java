90b7fbdcc86e62252834b2947777daed28ea8879
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-25 16:37:43 -0300

    Feat: add base implementation of EmailService using EmailRepository

--- src/main/java/com/ndrewcoding/mailmicroservice/services/EmailService.java
+++ src/main/java/com/ndrewcoding/mailmicroservice/services/EmailService.java
@@ -1,4 +1,15 @@
 package com.ndrewcoding.mailmicroservice.services;
 
+import com.ndrewcoding.mailmicroservice.repositories.EmailRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmailService {
+
+    private final EmailRepository emailRepository;
+
+    public EmailService(EmailRepository emailRepository) {
+        this.emailRepository = emailRepository;
+    }
+
 }
