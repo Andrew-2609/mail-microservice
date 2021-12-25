394b492cfbf469854529cf877031441701a32664
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-25 17:33:16 -0300

    Feat: implement method sendEmail with SimpleMailMessage

--- src/main/java/com/ndrewcoding/mailmicroservice/services/EmailService.java
+++ src/main/java/com/ndrewcoding/mailmicroservice/services/EmailService.java
@@ -1,8 +1,13 @@
 package com.ndrewcoding.mailmicroservice.services;
 
+import com.ndrewcoding.mailmicroservice.enums.EmailStatus;
 import com.ndrewcoding.mailmicroservice.models.EmailModel;
 import com.ndrewcoding.mailmicroservice.repositories.EmailRepository;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
@@ -13,7 +18,20 @@ public class EmailService {
         this.emailRepository = emailRepository;
     }
 
-    public void sendEmail(EmailModel emailModel) {
+    public EmailModel sendEmail(EmailModel emailModel) {
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
 
+            emailModel.setEmailStatus(EmailStatus.SENT);
+        } catch (MailException me) {
+            emailModel.setEmailStatus(EmailStatus.ERROR);
+        }
+
+        return emailRepository.save(emailModel);
     }
 }
